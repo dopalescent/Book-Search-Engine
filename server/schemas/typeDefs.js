40561f97a58f81e27cd0bdcc0c9337ccbf245044
dopalescent <skudlgre000@gmail.com>
2023-11-08 14:53:49 -0500

    wrote query and mutation resolvers in resolvers.js, incorporated graphql into server/utils/auth.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,7 +6,7 @@ const typeDefs = `
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(bookToSave: bookInput): User
+    saveBook(bookInput: bookInput): User
     removeBook(bookId: String!): User
   }
 
