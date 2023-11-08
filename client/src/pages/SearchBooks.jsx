ffd2b9f4edb46dc6ac40a00c2795c62ba0d8e0ca
dopalescent <skudlgre000@gmail.com>
2023-11-08 16:48:54 -0500

    incorporated SAVE_BOOK mutation into SearchBooks.jsx (I think?)

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -1,3 +1,5 @@
+import { useMutation } from "@apollo/client";
+import { SAVE_BOOK } from "../utils/mutations";
 import { useState, useEffect } from 'react';
 import {
   Container,
@@ -9,7 +11,7 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -20,6 +22,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -72,7 +76,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: {
+          input: bookToSave,
+        },
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
